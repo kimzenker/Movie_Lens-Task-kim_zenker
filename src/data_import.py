e8f45e1fff63ff9980c6326ecc9d415eb1e8c98f
Kim <43031838+kimzenker@users.noreply.github.com>
2025-07-07 16:41:07 +0200

    finished import

--- src/data_import.py
+++ src/data_import.py
@@ -1,1 +1,17 @@
-# import data
+import pandas as pd
+from pandas import DataFrame
+
+
+def import_users() -> DataFrame:
+    """returns df indexed by userid"""
+    return pd.read_csv('data/users.dat', sep='::', engine='python', names = ['uid', 'gender', 'age', 'occupation', 'zip-code' ], index_col='uid')    
+
+
+def import_movies() -> DataFrame:
+    """returns df indexed by movieid"""
+    return pd.read_csv('data/movies.dat', sep='::', engine='python', encoding='latin-1', names = ['mid', 'title', 'genres'])
+
+
+def import_ratings() -> DataFrame:
+    """returns dataframe"""
+    return pd.read_csv('data/ratings.dat', sep='::', engine='python', names = ['uid', 'mid', 'rating', 'timestamp'])
--- src/testing.py
+++ src/testing.py
@@ -1,2 +1,26 @@
-# manual tests for import 
-# some create some fake data for the functions
+import pytest
+from data_import import *
+## testing everything that went wrong for me while importing 
+def test_import_users():
+    users = import_users()
+    with pytest.raises(KeyError):
+        users.loc[0]
+    assert users.index[0] == 1, "First User ID should be 1 "
+    
+def test_import_movies():
+    movies = import_movies()
+    with pytest.raises(KeyError):
+        movies.loc[0]
+    assert movies.index[0] == 1, "First movie ID should be 1 "
+
+
+def test_import_ratings():
+    ratings = import_ratings()
+
+if __name__ == "__main__":
+    print(import_users().head())
+    print(import_movies().head())
+    print(import_ratings().head())
+    test_import_users()
+    test_import_movies()
+    test_import_ratings()
