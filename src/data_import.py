64c08537d620f77b8a4e1e20c25d39276588dfe8
Kim <github@zenker.kim>
2025-07-09 18:53:49 +0200

    added comments, replaced function with lambda

--- src/data_import.py
+++ src/data_import.py
@@ -3,11 +3,13 @@ from pandas import DataFrame
 
 
 def import_users(filepath) -> DataFrame:
-    """returns df indexed by userid"""
+    """
+    returns df indexed by userid (uid) with columns 'gender', 'age', 'occupation', 'zip-code'
+    """
     return pd.read_csv(
         filepath,
         sep="::",
-        engine="python",
+        engine="python",  # because of the '::' separator
         names=["uid", "gender", "age", "occupation", "zip-code"],
         index_col="uid",
         dtype={
@@ -21,36 +23,44 @@ def import_users(filepath) -> DataFrame:
 
 
 def import_movies(filepath) -> DataFrame:
-    """returns df indexed by movieid"""
+    """
+    returns df indexed by movieid (mid) with columns 'title' and 'genres'
+    """
     return pd.read_csv(
         filepath,
         sep="::",
-        engine="python",
-        encoding="latin-1",
+        engine="python",  # because of the '::' separator
+        encoding="latin-1",  # utf-8 doesn't work
         names=["mid", "title", "genres"],
         index_col="mid",
-        dtype={"mid": "int32", "title": "string", "genres": "string"},
+        dtype={
+            "mid": "int32",
+            "title": "string",
+            "genres": "string",  # genres aren't used for now so we keep them as string
+        },
     )
 
 
 def import_ratings(filepath) -> DataFrame:
-    """returns dataframe"""
-
-    def cast_ratings_to_float(rating):
-        ranting_for_float = str(rating).replace(",", ".")
-        return float(ranting_for_float)
-
+    """
+    returns df with columns 'uid', 'mid', 'rating', 'timestamp'
+    """
     return pd.read_csv(
         filepath,
         sep="::",
-        engine="python",
+        engine="python",  # because of the '::' separator
         names=["uid", "mid", "rating", "timestamp"],
-        converters={"rating": cast_ratings_to_float},
-        dtype={"uid": "int32", "mid": "int32", "timestamp": "string"},
+        converters={"rating": lambda x: float(x.replace(",", "."))},
+        dtype={
+            "uid": "int32",
+            "mid": "int32",
+            "timestamp": "string",  # timestamp is not used for now, so we keep it as string
+        },
     )
 
 
 age_group_translation = {
+    # taken from the dataset documentation
     1: "Under 18",
     18: "18-24",
     25: "25-34",
